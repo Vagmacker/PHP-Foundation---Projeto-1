f273b8b5dbdfade444b5203979402cf4000c92da
Joao Vagmacker <vagmackerdsm@gmail.com>
2016-01-12 16:03:57 -0200

    Modificando contato.php e excluindo footer.php

--- contato.php
+++ contato.php
@@ -67,7 +67,15 @@
         </fieldset>
     </form>
 
-    <?php include_once("footer.php"); ?>
-
+        <footer>
+            <div style="background-color: black">
+                <div class="container">
+                    <div class="row">
+                        <p class="text-center" style="color: white">Todos os direitos reservados - <?php echo date("Y"); ?> </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    
     </body>
 </html>
