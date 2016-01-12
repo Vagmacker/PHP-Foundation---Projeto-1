bc053f9f71294f9c3ccb7d36d2f68aae12c6acb2
Joao Vagmacker <vagmackerdsm@gmail.com>
2016-01-12 11:34:25 -0200

    Fazendo a pagina home no index

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="pt-br">
+    <head>
+        <title>Projeto1</title>
+
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+
+    </head>
+    <body>
+
+        <?php require_once("navegacao.php"); ?>
+
+        <div>
+            <h1 class="text-center home" style="font-size: 65px;">
+                Utroque maiestatis ius no<br/>Lorem ipsum dolor sit
+            </h1>
+        </div>
+    </body>
+</html>
