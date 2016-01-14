8ce0e7642d171227bce6799f82abb10633265820
Joao Vagmacker <vagmackerdsm@gmail.com>
2016-01-13 23:31:38 -0200

    Colocando inserindo arquivo 404.php

---
+++ 404.php
@@ -0,0 +1,6 @@
+
+<div>
+    <h1 class="text-center home" style="font-size: 65px;">
+        Nao existe esse arquivo
+    </h1>
+</div>
---
+++ home.php
@@ -0,0 +1,5 @@
+<div>
+    <h1 class="text-center home" style="font-size: 65px;">
+        Home
+    </h1>
+</div>
